72a768e40fbb59cf8936597fd6d6f320b4fb1a37
Srevuplo <bleylmax@icloud.com>
2025-12-14 22:56:43 +0100

    Update bannedWords.js

--- bannedWords.js
+++ bannedWords.js
@@ -54,7 +54,7 @@ const WHITELIST = [
   "aromantic", "demisexual", "sapiosexual",
   "dickinson", "cocktail", "breastplate", "scunthorpe",
   "cucumber", "assessor", "succumb",
-  "essex", "essexshire", "https", "fa", "suspicious", "grapes"
+  "essex", "essexshire", "https", "fa", "suspicious", "grapes", "document", "circumstances"
 ];
 
 // BASE CONFIG
